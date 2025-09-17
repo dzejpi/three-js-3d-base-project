7c38ee3c4649ecc0c051f6171decb096c3c628fd
DzejPi <dzejjpii@gmail.com>
2025-09-17 21:25:22 +0200

    Added isDebug into the App so that it's possible to just jump right into the game.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import MainMenu from "./scenes/MainMenu";
 import SplashScreen from "./scenes/SplashScreen";
 
 type Scene = "splash" | "menu" | "game";
+let isDebug: boolean = false;
 
 export default function App() {
-    const [scene, setScene] = useState<Scene>("splash");
+    const [scene, setScene] = useState<Scene>(isDebug ? "game" : "splash");
 
     return (
         <>
