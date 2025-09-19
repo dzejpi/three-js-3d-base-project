390c791395673f461e37aff4dac1a4f50c3809b6
DzejPi <dzejjpii@gmail.com>
2025-09-19 19:26:40 +0200

    Added a very basic main menu.

--- src/scenes/MainMenu.tsx
+++ src/scenes/MainMenu.tsx
@@ -5,9 +5,38 @@ interface Props {
 }
 
 export default function MainMenu({ onStart }: Props) {
+    const handleNewGame = () => {
+        onStart();
+    };
+    
+    const handleOptions = () => {
+        console.log("Options TODO");
+    };
+
+    const handleSounds = () => {
+        console.log("Sounds TODO");
+    };
+
+    const handleMusic = () => {
+        console.log("Music TODO");
+    };
+
+    const handleCredits = () => {
+        console.log("Credits TODO");
+    };
+
+    const handleQuit = () => {
+        console.log("Quit game TODO");
+    };
+
     return (
-        <div>
-            <p>This is a main menu.</p>
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem", alignItems: "center", marginTop: "2rem" }}>
+            <button onClick={handleNewGame}>Start</button>
+            <button onClick={handleOptions}>Options</button>
+            <button onClick={handleSounds}>Sounds: on</button>
+            <button onClick={handleMusic}>Music: on</button>
+            <button onClick={handleCredits}>Credits</button>
+            <button onClick={handleQuit}>Quit</button>
         </div>
     );
 }
