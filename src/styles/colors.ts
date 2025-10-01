9254f55a54c1d57ae03883098fa109dc53aea049
DzejPi <dzejjpii@gmail.com>
2025-10-01 20:09:59 +0200

    Super basic button color tokens added.

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+const button_disabled = '#9e9e9e'
+const button_edge = '#000000'
+const button_focused = '#37474f'
+const button_hover = '#455a64'
+const button_pressed = '#546e7a'
+const button_primary = '#90a4ae'
+const button_text = '#ffffff'
+
+export const ColorTokens = {
+    button_disabled,
+    button_edge,
+    button_focused,
+    button_hover,
+    button_pressed,
+    button_primary,
+    button_text,
+}
