75482ec3dd3d2cf36c739f3d2d768238343ad283
dzejpi <dzejjpii@gmail.com>
2025-10-04 00:21:44 +0200

    Moved Player components into the basic Game component for increased modularity.

--- src/scenes/Game.tsx
+++ src/scenes/Game.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import TestingWorld from "../world/TestingWorld";
 import PauseMenu from "./PauseMenu";
 import GeneralGameButton from "../ui/GeneralGameButton";
+import PlayerController from "../player/PlayerController";
+import { Physics } from "@react-three/rapier";
+import { Canvas } from "@react-three/fiber";
 
 interface Props {
     onExit: () => void;
@@ -31,18 +34,33 @@ export default function Game({ onExit }: Props) {
 
     return (
         <div style={{ width: "100vw", height: "100vh" }}>
-        {/* The 3D test world */}
-        <TestingWorld onStart={() => {}} />
+        
+        <Canvas
+            camera={{ position: [0, 2, 5], fov: 75 }}
+            style={{ width: "100vw", height: "100vh", background: "skyblue" }}
+            >
+
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[5, 10, 5]} />
+            
+            <Physics gravity={[0, -9.81, 0]}>
+                {/* The 3D test world */}
+                <TestingWorld onStart={() => {}} />
+
+                {/* Player controller */}
+                <PlayerController />
+            </Physics>
+        </Canvas>
 
         {/* Pause overlay */}
         {paused && (
             <PauseMenu
-            onResume={() => {
-                setPaused(false);
-                const canvas = document.querySelector("canvas");
-                if (canvas) canvas.requestPointerLock();
-            }}
-            onMainMenu={onExit}
+                onResume={() => {
+                    setPaused(false);
+                    const canvas = document.querySelector("canvas");
+                    if (canvas) canvas.requestPointerLock();
+                }}
+                onMainMenu={onExit}
             />
         )}
 
--- src/world/TestingWorld.tsx
+++ src/world/TestingWorld.tsx
@@ -1,41 +1,28 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { Physics, RigidBody } from "@react-three/rapier";
-import PlayerController from "../player/PlayerController";
+import { RigidBody } from "@react-three/rapier";
 
 interface Props {
     onStart: () => void;
 }
 
 export default function TestingWorld({ onStart }: Props) {
-    return (
-        <Canvas
-            camera={{ position: [0, 2, 5], fov: 75 }}
-            style={{ width: "100vw", height: "100vh", background: "skyblue" }}
-            >
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[5, 10, 5]} />
-
-            <Physics gravity={[0, -9.81, 0]}>
-                {/* Ground plane */}
-                <RigidBody type="fixed">
+    return (        
+        <>
+            {/* Ground plane */}
+            <RigidBody type="fixed">
                 <mesh receiveShadow>
                     <boxGeometry args={[20, 1, 20]} />
                     <meshStandardMaterial color="green" />
                 </mesh>
-                </RigidBody>
+            </RigidBody>
 
-                {/* Test cube */}
-                <RigidBody colliders="cuboid" position={[0, 2, 0]}>
+            {/* Test cube */}
+            <RigidBody colliders="cuboid" position={[0, 2, 0]}>
                 <mesh castShadow>
                     <boxGeometry args={[1, 1, 1]} />
                     <meshStandardMaterial color="lightgrey" />
                 </mesh>
-                </RigidBody>
-
-                {/* Player controller */}
-                <PlayerController />
-            </Physics>
-        </Canvas>
+            </RigidBody>
+        </>
     );
 }
