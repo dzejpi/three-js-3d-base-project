8565332784999ecd8033e0d43da9a89872acfa87
DzejPi <dzejjpii@gmail.com>
2025-12-18 13:26:45 +0100

    Game UI title using Tailwind.

--- src/ui/GameUiTitle.tsx
+++ src/ui/GameUiTitle.tsx
@@ -5,5 +5,5 @@ interface GameUiTitleProps {
 }
 
 export default function GameUiTitle({ children }: GameUiTitleProps) {
-	return <h1 style={{ marginBottom: '2rem' }}>{children}</h1>;
+	return <h1 className="text-4xl text-center mb-2">{children}</h1>;
 }
